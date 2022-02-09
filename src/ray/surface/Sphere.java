3c0671d9e21641bb823999d78efbd88908b44d16
cavenderbi <cavenderbi@appstate.edu>
2022-02-09 13:03:28 -0500

    Sphere normals implemented and passing tests.

--- src/ray/surface/Sphere.java
+++ src/ray/surface/Sphere.java
@@ -21,14 +21,6 @@ public class Sphere extends Surface {
 	public Sphere() { }
 
 	public HitRecord hit(Vector3 direction, Point3 origin) {
-		/*	(ray dot ray)t^2 + (2viewpoint dot ray)*t + (viewpoint dot viewpoint - 2viewpoint dot center + center dot center - radius^2) 
-			(-B +/- sqrt(B^2-4AC))/2A
-
-			((2viewpoint dot ray) +/- sqrt((2viewpoint dot ray)^2 - 4(ray dot ray)(viewpoint dot viewpoint - 2viewpoint dot center + center dot center - radius^2))/2(ray dot ray)
-			Returns NaN if ray is perpendicular to itself (not possible) or the ray misses the surface completely. 
-
-			Normal: 2(q-c)
-		*/
 		Vector3 oc = new Vector3();
 		oc.sub(origin, center);
 
@@ -52,42 +44,21 @@ public class Sphere extends Surface {
 		}
 
 		Vector3 normal = new Vector3(); 
-		Point3 hitpoint = new Point3();
+		Point3 hitpoint = new Point3(origin); // o + td
 		hitpoint.scaleAdd(t, direction);
-		normal.sub(origin, hitpoint);
+		normal.sub(hitpoint, center); // Vector from center to hitpoint. 
+		normal.normalize();
 
 		if (t > 0) 
 			return new HitRecord(this, t, normal);
 		else return null;
-
-		/* Vector3 oc = new Vector3();
-		oc.sub(viewpoint, center);
-
-		double A = ray.dot(ray);
-		double B = 2 * ray.dot(oc);
-		double C = oc.dot(oc) - Math.pow(radius, 2);
-
-		double t_plus = (-B + Math.sqrt(Math.pow(B, 2) - 4 * A * C)) / (2 * A);
-		double t_minus = (-B - Math.sqrt(Math.pow(B, 2) - 4 * A * C)) / (2 * A);
-		double t;
-		if (t_plus > 0 && t_plus < t_minus) t = t_plus;
-		else if (t_minus > 0) t = t_minus;
-		else return null;
-
-		Vector3 normal = new Vector3();
-		viewpoint.scaleAdd(t, ray); // o + td = q
-		normal.sub(viewpoint, center); // (q - c)
-		normal.scale(2); // 2 * (q - c) is the normal. 
-
-		return new HitRecord(this, t, normal); */
 	}
 	
 	/**
 	 * @see Object#toString()
 	 */
 	public String toString() {
-		
+
 		return "sphere " + center + " " + radius + " " + material + " end";
-	}
-	
+	}	
 }
